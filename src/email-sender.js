2e81784cc6213be16097d4984df9943ec5966398
Ken <kenovienadu@gmail.com>
2025-02-25 15:39:20 +0100

    feat: implement task 5

---
+++ src/email-sender.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+
+// helps to load the env variables from a .env file into process.env
+dotenv.config();
+
+const sendMail = () => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.RECEIVER_EMAIL,
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
